e0c51fd64894df43de6caa73f9daf3c81ef16b05
Srikalyan Chandrashekar <srikalyan@yahoo-inc.com>
2014-04-05 18:52:15 -0700

    Licence changes.

--- src/bmdiff/test/SimpleTests.java
+++ src/bmdiff/test/SimpleTests.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Yahoo! Inc. Licensed under the Apache License, Version 2.0 (the
+ * Copyright 2014 Srikalyan Chandrashekar. Licensed under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
  * Unless required by applicable law or agreed to in writing, software distributed
